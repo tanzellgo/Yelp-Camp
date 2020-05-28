71966d0e38b05e4a0a5aee29008e53bc83de7ff5
tanzellgo <tanzell.go@obf.ateneo.edu>
2020-05-28 11:28:36 +0800

    db resolve 2

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 
 
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, process.env.IP, () => {
     console.log("Server has started!")
 });
